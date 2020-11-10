3e5df5f7b92e779c220a7b278578adf13d5b168b
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2020-11-10 15:21:33 +0100

    feat(options): Add Options panel to fulfill stock exchange values

--- main.js
+++ main.js
@@ -1,71 +1,62 @@
-// stock_array = document.getElementsByClassName("c-shadow-overflow");
-// stock_row_first = stock_array[0].getElementsByClassName("c-table__body")[0]["children"];
-// stock_cell = stock_row_first[0].getElementsByClassName("c-table__cell");
-// stock_title = stock_cell[0].getElementsByClassName("o-pack__item");
-// stock_cell_add = stock_row_first[0].insertCell(1);
+/*
+    Init variables
+*/
+/* Stock Exchange MyValues */
+//     Values are retrieved from the Extension Options
+//     manually inserted from another trading platform.
+stock_my_values = {}
 
-// stock_title = stock_title[0].innerText;
-// stock_cell_add.innerHTML = stock_title;
-
-stock_my_values = {
-    "BOUYGUES": 30.16,
-    "CREDIT AGRICOLE SA": 7.10,
-    "EDF": 9.94,
-    "ENGIE": 11.01,
-    "FAURECIA": 32.73,
-    "FDJ": 31.60,
-    "GENFIT": 3.39,
-    "M6 METROPOLE TELE.": 10.22,
-    "NACON": 7.26,
-    "NATIXIS": 1.99,
-    "ORANGE": 9.21,
-    "PEUGEOT": 15.14,
-    "SAINT-GOBAIN": 33.76,
-    "SOCIETE GENERALE": 11.84,
-    "SUEZ": null,
-    "THALES": 60.55,
-    "TOTAL": 27.30,
-    "VEOLIA": 17.49
-}
-
-stock_table = document.getElementsByClassName("c-table")[0];
-stock_head_cell_value = stock_table.tHead.children[0];
-stock_head_cell_value.insertCell(1).outerHTML = '<th class="c-table__cell c-table__cell--head c-table__cell--dotted / u-text-uppercase c-table__cell--sorted"><h3 class="c-table__title">My data</h3></th>';
-stock_head_cell_value.insertCell(2).outerHTML = '<th class="c-table__cell c-table__cell--head c-table__cell--dotted / u-text-uppercase c-table__cell--sorted"><h3 class="c-table__title">My perc</h3></th>';
-
-
-stock_rows = stock_table.tBodies[0].children;
-console.log(stock_rows);
-stock_rows_length = stock_rows.length;
-for ( i=0 ; i<stock_rows_length ; i++ ) {
-    stock_title = stock_rows[i].children[0].children[0].children[0].children[0].innerHTML;
-    stock_value = Number(stock_rows[i].children[1].innerHTML);
-    stock_my = stock_my_values[stock_title];
-    stock_my_cell = stock_rows[i].insertCell(1);
-    stock_perc_cell = stock_rows[i].insertCell(2);
-        
-    if (stock_title in stock_my_values && stock_my != null) {
-        value_sort = stock_my_values[stock_title];
-        value_data = stock_my_values[stock_title];
-        perc_sort = stock_my_values[stock_title];
-        perc_data =  Number.parseFloat((stock_value/stock_my-1)*100).toPrecision(2) + '%';
-        if (stock_my < stock_value) {
-            if (stock_my < stock_value*(1-0.008)) {
-                stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#2cc357;">' + value_data + '</div></td>';
-                stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '"><div style="color:#2cc357;">' + perc_data + '</div></td>';
-            } else {
-                stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#FFA500;">' + value_data + '</div></td>';
-                stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '"><div style="color:#FFA500;">' + perc_data + '</div></td>';
-            }
-        } else if (stock_my > stock_value) {
-            stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#f11c3a;">' + value_data + '</div></td>';
-            stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#f11c3a;">' + perc_data + '</div></td>';
-        } else {
-            stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '">' + value_data + '</td>';
-            stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '">' + perc_data + '</td>';
+// Retrieve MyValue from Extension Options
+chrome.storage.sync.get({
+    stockExchange: null,
+    }, function(items) {
+        for (const [stock_name, stock_value] of Object.entries(items["stockExchange"])) {
+            // Update array with values stored in sync storage
+            stock_my_values[stock_name] = stock_value;
         }
-    } else {
-        stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted "></td>';
-        stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted "></td>';
+
+        // Update HTML body
+        stock_table = document.getElementsByClassName("c-table")[0];
+        stock_head_cell_value = stock_table.tHead.children[0];
+        // Define table header
+        stock_head_cell_value.insertCell(1).outerHTML = '<th class="c-table__cell c-table__cell--head c-table__cell--dotted / u-text-uppercase c-table__cell--sorted"><h3 class="c-table__title">My data</h3></th>';
+        stock_head_cell_value.insertCell(2).outerHTML = '<th class="c-table__cell c-table__cell--head c-table__cell--dotted / u-text-uppercase c-table__cell--sorted"><h3 class="c-table__title">My perc</h3></th>';
+
+        // Define table rows
+        stock_rows = stock_table.tBodies[0].children;
+        stock_rows_length = stock_rows.length;
+        for ( i=0 ; i<stock_rows_length ; i++ ) {
+            stock_title = stock_rows[i].children[0].children[0].children[0].children[0].innerHTML;
+            stock_value = Number(stock_rows[i].children[1].innerHTML);
+            stock_my = stock_my_values[stock_title];
+            stock_my_cell = stock_rows[i].insertCell(1);
+            stock_perc_cell = stock_rows[i].insertCell(2);
+                
+            if (stock_title in stock_my_values && stock_my != null) {
+                value_sort = stock_my_values[stock_title];
+                value_data = stock_my_values[stock_title];
+                perc_sort = stock_my_values[stock_title];
+                perc_data =  Number.parseFloat((stock_value/stock_my-1)*100).toPrecision(2) + '%';
+                if (stock_my < stock_value) {
+                    if (stock_my < stock_value*(1-0.008)) {
+                        stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#2cc357;">' + value_data + '</div></td>';
+                        stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '"><div style="color:#2cc357;">' + perc_data + '</div></td>';
+                    } else {
+                        stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#FFA500;">' + value_data + '</div></td>';
+                        stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '"><div style="color:#FFA500;">' + perc_data + '</div></td>';
+                    }
+                } else if (stock_my > stock_value) {
+                    stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#f11c3a;">' + value_data + '</div></td>';
+                    stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '"><div style="color:#f11c3a;">' + perc_data + '</div></td>';
+                } else {
+                    stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + value_sort + '">' + value_data + '</td>';
+                    stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted " data-sort-value="' + perc_sort + '">' + perc_data + '</td>';
+                }
+            } else {
+                stock_my_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted "></td>';
+                stock_perc_cell.outerHTML = '<td class="c-table__cell c-table__cell--dotted "></td>';
+            }
+        }
+
     }
-}
+);
--- options.js
+++ options.js
@@ -1,1 +1,70 @@
+function add_table_row_empty(id) {
+    currentTable = document.getElementById("stock-table");
+    newLine = currentTable.insertRow();
 
+    newCellKey = newLine.insertCell(0);
+    newCellKeyInput = document.createElement("input");
+    newCellKey.appendChild(newCellKeyInput);
+    newCellKeyInput.setAttribute("id", "stock_"+id+"_key")
+
+    newCellValue = newLine.insertCell(1);
+    newCellValueInput = document.createElement("input");
+    newCellValue.appendChild(newCellValueInput);
+    newCellValueInput.setAttribute("id", "stock_"+id+"_val")
+}
+
+function save_options() {
+    data = {}
+    count_input = 0
+    while(count_input >= 0) {
+        element_key = document.getElementById('stock_'+count_input+'_key');
+        element_value = document.getElementById('stock_'+count_input+'_val');
+        if (element_key == null) {
+            count_input = -1;
+            break;
+        } else if(element_value.value == "") {
+            count_input = -1;
+            break;
+        }
+        data[element_key.value] = element_value.value;
+        count_input = count_input+1;
+    }
+
+    chrome.storage.sync.set({
+        stockExchange: data
+    }, function() {
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+  
+function restore_options() {
+    chrome.storage.sync.get({
+        stockExchange: {}
+    }, function(items) {
+        stockEchangeLength = Object.keys(items["stockExchange"]).length
+        for (const [index, [stock_name, stock_value]] of Object.entries(Object.entries(items["stockExchange"]))) {
+            if (document.getElementById('stock_'+index+'_key') == null) {
+                add_table_row_empty(index)
+            }
+            document.getElementById('stock_'+index+'_key').value = stock_name;
+            document.getElementById('stock_'+index+'_val').value = stock_value;
+        }
+    });
+}
+
+
+function add_table_value() {
+    currentInputs = document.getElementsByTagName("input")
+    nextInputId = currentInputs.length/2
+
+    add_table_row_empty(nextInputId)
+}
+
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('add').addEventListener('click', add_table_value);
